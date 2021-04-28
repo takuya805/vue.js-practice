f85e0d27e01bb1cfea77ce4c1326838390b49fda
takuya805 <takuya0903exile@gmail.com>
2021-04-28 23:10:17 +0900

    Javascript on vue.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 var app = new Vue({
   el: '#app',
-  data:{
-    message: 'Hello <span style="color:red;">Vue.js!</span>'
-  },
-  methods:{
-    clickHandler: function(event){
-      this.message=this.message.split('').reverse().join('')
-    }
+  data: {
+    message: 'Hello Vue.js!',
+    number: 100,
+    ok: false
   }
 })
